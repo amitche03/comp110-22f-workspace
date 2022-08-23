1ceaf86b00f3e40e48fd43c39217c48e3426234e
Mitchell Anderson <mitchell-s-anderson@unc.edu>
2022-08-22 22:12:12 -0400

    Chardle Step 2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,18 +6,24 @@ WORD: str = input("Enter a 5-character word: ")
 character: str = input("Enter a single character: ")
 print("Searching for " + character + " in " + WORD )
 
-if character == WORD[0]:
-    print( WORD[0] + " found at index 0")
-else:
-   print("No instances of " + character + " found in " + WORD)
+if character in WORD:
+    if character == WORD[0]:
+        print( WORD[0] + " found at index 0")
+    
+    if character == WORD[1]:
+        print(WORD[1] + " found at index 1")
 
-if character == WORD[1]:
-    print( WORD[1] + " found at index 1")
-if character == WORD[2]:
-    print( WORD[2] + " found at index 2")
-if character == WORD[3]:
-    print (WORD[3] + " found at index 3")
-if character == WORD[4]: 
-    print(WORD[4] + " found at index 4")
+    if character == WORD[2]:
+        print(WORD[2] + " found at index 2")
+
+    if character == WORD[3]:
+        print(WORD[3] + " found at index 3")
+
+    if character == WORD[4]:
+        print(WORD[4] + " found at index 4")
+else:
+    print("No instances of " + character + " found in " + WORD)
+
+
 
 
