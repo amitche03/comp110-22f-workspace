75f2be8a256fa220e262bacacdddfa4788c77d65
Mitchell Anderson <mitchell-s-anderson@unc.edu>
2022-08-23 13:49:22 -0400

    Chardle Progress, Finished Step 3

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,28 +1,37 @@
-"""EX01 - Chardle - A cute step toward Wordle."""
+"""EX01 - Chardle - A cute step towards Wordle."""
 
 __author__ = "730562021"
 
 WORD: str = input("Enter a 5-character word: ")
-character: str = input("Enter a single character: ")
-print("Searching for " + character + " in " + WORD )
+user_character: str = input("Enter a single character: ")
+matching_characters: int = 0
+print("Searching for " + user_character + " in " + WORD )
 
-if character in WORD:
-    if character == WORD[0]:
+if user_character == WORD[0]:
         print( WORD[0] + " found at index 0")
+        matching_characters += 1
     
-    if character == WORD[1]:
+if user_character == WORD[1]:
         print(WORD[1] + " found at index 1")
+        matching_characters += 1
 
-    if character == WORD[2]:
+if user_character == WORD[2]:
         print(WORD[2] + " found at index 2")
+        matching_characters += 1
 
-    if character == WORD[3]:
+if user_character == WORD[3]:
         print(WORD[3] + " found at index 3")
+        matching_characters += 1
 
-    if character == WORD[4]:
+if user_character == WORD[4]:
         print(WORD[4] + " found at index 4")
-else:
-    print("No instances of " + character + " found in " + WORD)
+        matching_characters += 1
+
+if matching_characters == 0:
+     print("No instances of " + user_character + " found in " + WORD)
+else: 
+    print(str(matching_characters) + " instances of " + user_character + " found in " + WORD)
+
 
 
 
