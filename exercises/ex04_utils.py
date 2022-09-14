28c36d2113e32635548ec258eada12cc8543b7a4
Mitchell Anderson <mitchell-s-anderson@unc.edu>
2022-09-14 15:50:58 -0400

    ex04 Part 1 and 2

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -7,15 +7,37 @@ def all(given_list: list[int], searched_number: int) -> bool:
     is_same: bool = True
     while i <= len(given_list) - 1 and is_same:
         if given_list[i] != searched_number:
-            is_same == False
+            is_same = False
         i += 1
     return is_same
 
 def max(max_list: list[int]) -> int:
     if len(max_list) == 0:
         raise ValueError("max() arg is an empty list")
-        i: int = 0
-    while i <= len(max_list):
+    i: int = 0
+    largest_int: int = max_list[0]
+    while i <= len(max_list) - 1:
+        if largest_int < max_list[i]:
+            largest_int = max_list[i]
+        i += 1
+    return largest_int
+
+def is_equal(first_list: list[int], second_list: list[int]) -> bool:
+    i: int = 0
+    is_same: bool = True 
+    while i <= len(first_list) - 1 and is_same:
+        if first_list[i] != second_list[i]:
+            is_same = False
+        i += 1
+    return is_same
+
+
+
+        
+
+
+
+
 
 
     
